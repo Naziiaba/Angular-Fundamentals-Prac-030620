483e360f942ee4269fda9b8b3f8594f278ef947e
Zia pmr <gernersnc@gmail.com>
2020-03-06 15:53:14 -0600

    Interpolation

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,14 +2,26 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `<div>Inline text</div>`,
+  template: `
+  <div> {{name}} Working on a new project </div>
+  <h2>{{2+2}}</h2>
+  <h2> {{"Welcome " + name}}</h2>
+  <h2> {{name.length}}</h2>
+  <h2>{{name.toUpperCase()}}</h2>
+  <h2> {{greetUser()}}
+  `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
+  public name = "Jefferson";
   constructor() { }
 
   ngOnInit() {
+  }
+
+  greetUser(){
+    return "Hello " + this.name;
   }
 
 }
