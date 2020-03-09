4bd928d14e5de4257924ac488c2cdcb5b8d7f529
Naziia Travels <35944002+Naziiaba@users.noreply.github.com>
2020-03-08 20:16:00 -0700

    Update test.component.ts

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-  <div> {{name}} Working on a new project </div>
   <h2>{{2+2}}</h2>
   <h2> {{"Welcome " + name}}</h2>
   <h2> {{name.length}}</h2>
